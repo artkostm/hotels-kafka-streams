07f8d10c530198a036911398b4f7b28f7f77576e
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-20 18:40:48 +0300

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,14 @@ import sbt._
 object Dependencies {
   val versions = new {
     val randomDataGenerator = "2.6"
+    val alpakka = "0.22"
   }
 
   lazy val common = Seq(
     )
 
   lazy val generatorModule = Seq(
-    "com.danielasfregola" %% "random-data-generator" % versions.randomDataGenerator
+    "com.danielasfregola" %% "random-data-generator" % versions.randomDataGenerator,
+    "com.typesafe.akka" %% "akka-stream-kafka" % versions.alpakka
   )
 }
