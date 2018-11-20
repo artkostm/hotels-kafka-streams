afd953ad8af765d25321b154f05cbeaf69c6d899
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-20 18:41:26 +0300

    small example

--- generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Main.scala
+++ generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Main.scala
@@ -1,5 +1,19 @@
 package by.artsiom.bigdata101.hotels.generator
 
-object Main {
+import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.stream.scaladsl.{Sink, Source}
 
+object Main extends App {
+  implicit val system = ActorSystem("hotel_events_generator")
+  implicit val actorMaterializer = ActorMaterializer()
+
+//  val producerSettings = ProducerSettings(system, None, None).withParallelism(10)
+
+  var count = 0
+  Source
+    .fromPublisher(RandomEventsPublisher(100000))
+    .to(Sink.foreach(e => {count = count + 1;println(s"$count - $e")}))
+    .run()
 }
