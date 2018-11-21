2325ddc484fc625bc72f3c8083e8f565783203dd
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-21 16:52:42 +0300

    add configuration

--- generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/config/Configuration.scala
+++ generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/config/Configuration.scala
@@ -1,5 +1,24 @@
 package by.artsiom.bigdata101.hotels.generator.config
 
-object Configuration {
+import akka.actor.{
+  ExtendedActorSystem,
+  Extension,
+  ExtensionId,
+  ExtensionIdProvider
+}
+import com.typesafe.config.Config
 
+private[config] class ConfigurationImpl(config: Config) extends Extension {
+  val NumberOfEvents = config.getInt("generator.number-of-events")
+  val Parallelism = config.getInt("akka.kafka.producer.parallelism")
+  val TopicName = config.getString("akka.kafka.producer.topic.name")
+}
+
+object Configuration
+    extends ExtensionId[ConfigurationImpl]
+    with ExtensionIdProvider {
+  override def createExtension(system: ExtendedActorSystem): ConfigurationImpl =
+    new ConfigurationImpl(system.settings.config)
+
+  override def lookup(): ExtensionId[_ <: Extension] = Configuration
 }
