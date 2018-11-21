545343230ff1b130dd1a668ea5bb02056777bee4
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-21 16:53:01 +0300

    update main

--- generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Main.scala
+++ generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Main.scala
@@ -1,19 +1,48 @@
 package by.artsiom.bigdata101.hotels.generator
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorSystem
 import akka.kafka.ProducerSettings
-import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
-import akka.stream.scaladsl.{Sink, Source}
+import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink}
+import by.artsiom.bigdata101.hotels.generator.converter.EventConverter
+import by.artsiom.bigdata101.hotels.generator.publisher.RandomEventsPublisher
+import by.artsiom.bigdata101.hotels.model.Event
+import org.apache.kafka.common.serialization.StringSerializer
 
-object Main extends App {
-  implicit val system = ActorSystem("hotel_events_generator")
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+import scala.util.{Failure, Success}
+
+object Main extends App with Generator with ConfigurationAware {
+  override protected implicit val system: ActorSystem = ActorSystem(
+    "hotel_events_generator")
+
   implicit val actorMaterializer = ActorMaterializer()
+  implicit val global = system.dispatcher
 
-//  val producerSettings = ProducerSettings(system, None, None).withParallelism(10)
+  val producerSettings = ProducerSettings[String, String](system,
+                                                          new StringSerializer,
+                                                          new StringSerializer)
 
-  var count = 0
-  Source
-    .fromPublisher(RandomEventsPublisher(100000))
-    .to(Sink.foreach(e => {count = count + 1;println(s"$count - $e")}))
-    .run()
+  val producerRecordFlow = Flow.fromFunction[Event, Message](EventConverter(topic()))
+
+  val doneFuture = generate(RandomEventsPublisher(numberOfEvents()),
+                            producerRecordFlow,
+                            Sink.foreach(println))
+
+//    .runWith(Producer.plainSink(producerSettings))
+//    .runWith(Sink.foreach(_ => ()))
+
+  doneFuture.onComplete(done => {
+    done match {
+      case Success(value)     => println(value)
+      case Failure(exception) => exception.printStackTrace()
+    }
+    system.terminate()
+  })
+
+  Await.ready(system.whenTerminated, Duration(1, TimeUnit.MINUTES))
 }
