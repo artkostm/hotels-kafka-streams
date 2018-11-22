fbbefa2b9221b88117fb4c73339547bcd249a89e
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-22 18:05:57 +0300

    add streaming entry point

--- streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
+++ streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
@@ -1,5 +1,32 @@
 package by.artsiom.bigdata101.hotels.streaming
 
-class Main {
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
+
+object Main extends App {
+
+
+  val spark = SparkSession.builder.master("local[*]")
+    .appName("hotels-test").getOrCreate()
+
+  import spark.implicits._
+
+  val records = spark.readStream
+    .format("kafka")
+    .option("subscribe", "TestTopic")
+    .option("kafka.bootstrap.servers", "192.168.99.100:9092")
+    .load().selectExpr("CAST(value AS STRING)").as[String]
+
+  val sq = records.flatMap(_.split(" ")).groupBy("value").count().
+    writeStream
+    .outputMode("complete")
+    .format("console")
+    .start()
+
+  sq.awaitTermination()
+  sq.stop()
+
+  spark.stop()
 }
