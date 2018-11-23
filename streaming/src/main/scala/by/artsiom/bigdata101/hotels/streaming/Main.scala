df6b2b21a2d96d00967dfd8c9c11f83c803c71ae
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-23 13:32:31 +0300

    fix formatting

--- streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
+++ streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
@@ -4,12 +4,12 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
-
 object Main extends App {
 
-
-  val spark = SparkSession.builder.master("local[*]")
-    .appName("hotels-test").getOrCreate()
+  val spark = SparkSession.builder
+    .master("local[*]")
+    .appName("hotels-test")
+    .getOrCreate()
 
   import spark.implicits._
 
@@ -17,10 +17,15 @@ object Main extends App {
     .format("kafka")
     .option("subscribe", "TestTopic")
     .option("kafka.bootstrap.servers", "192.168.99.100:9092")
-    .load().selectExpr("CAST(value AS STRING)").as[String]
+    .load()
+    .selectExpr("CAST(value AS STRING)")
+    .as[String]
 
-  val sq = records.flatMap(_.split(" ")).groupBy("value").count().
-    writeStream
+  val sq = records
+    .flatMap(_.split(" "))
+    .groupBy("value")
+    .count()
+    .writeStream
     .outputMode("complete")
     .format("console")
     .start()
