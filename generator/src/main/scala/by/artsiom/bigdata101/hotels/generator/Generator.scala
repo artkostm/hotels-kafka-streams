d64d77c27e04ba6458e395cfd6ae6b98302d938c
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-23 13:30:18 +0300

    update generator

--- generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Generator.scala
+++ generator/src/main/scala/by/artsiom/bigdata101/hotels/generator/Generator.scala
@@ -3,26 +3,22 @@ package by.artsiom.bigdata101.hotels.generator
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream._
-import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.scaladsl.{GraphDSL, RunnableGraph}
 import by.artsiom.bigdata101.hotels.generator.config.Configuration
 import by.artsiom.bigdata101.hotels.model.Event
-import org.reactivestreams.Publisher
-import scala.concurrent.duration._
 
 trait Generator {
   protected val system: ActorSystem
 
-  def generate(eventsPublisher: Publisher[Event],
+  def generate(eventSource: Graph[SourceShape[Event], NotUsed],
                eventConverter: Graph[FlowShape[Event, Message], NotUsed],
                sink: Graph[SinkShape[Message], Mat])(
-      implicit materializer: ActorMaterializer): Mat =
-    Source
-      .fromPublisher(eventsPublisher)
-      .throttle(10, 5 seconds) // for testing only
-//    .async
-//    .buffer(10000, OverflowStrategy.backpressure)
-      .via(eventConverter)
-      .runWith(sink)
+      implicit materializer: ActorMaterializer): RunnableGraph[Mat] =
+    RunnableGraph.fromGraph(GraphDSL.create(sink) { implicit builder => s =>
+      import GraphDSL.Implicits._
+      eventSource ~> eventConverter ~> s.in
+      ClosedShape
+    })
 }
 
 trait ConfigurationAware { self: Generator =>
