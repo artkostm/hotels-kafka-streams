77a92f50370b671ffc14987d1e8716bcf7946248
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-26 17:09:51 +0300

    update streaming

--- streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
+++ streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
@@ -1,24 +1,18 @@
 package by.artsiom.bigdata101.hotels.streaming
 
-import java.io.ByteArrayInputStream
-
-import by.artsiom.bigdata101.hotels.model.Event
-import com.sksamuel.avro4s.{AvroInputStream, AvroSchema}
-import org.apache.avro.Schema
+import by.artsiom.bigdata101.hotels.HotelImplicits
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.types.BinaryType
 
-object Main extends App {
+object Main extends App with HotelImplicits {
 
   args match {
     case Array(brockerList, topicName, outputDir) =>
-      val spark = SparkSession.builder
+      implicit val spark = SparkSession.builder
         .master("local[*]")
         .appName("hotels-streaming")
         .getOrCreate()
-
-      val eventCountAcc = spark.sparkContext.longAccumulator("event_count")
 
       val kafkaStreamDF = spark.readStream
         .format("kafka")
@@ -28,21 +22,11 @@ object Main extends App {
         .load()
 
       import spark.implicits._
-      import Event._
-
-      implicit val eventSchema = AvroSchema[Event]
-
-      val rowEventSchema = eventSchema.toString
 
       kafkaStreamDF
         .coalesce(2)
         .select($"value".cast(BinaryType).as("event"))
-        .map(row => {
-          val bais = new ByteArrayInputStream(row.getAs[Array[Byte]]("event"))
-          val input = AvroInputStream.binary[Event].from(bais).build(new Schema.Parser().parse(rowEventSchema))
-          eventCountAcc.add(1)
-          input.iterator.next()
-        })
+        .fromAvro("event")
         .writeStream
         .format("parquet")
         .queryName("parquet_files_to_hdfs")
@@ -52,6 +36,8 @@ object Main extends App {
         .start()
         .awaitTermination()
     case _ =>
-      sys.error("Usage: spark-submit --class Main --master <master> streaming.jar <brocker-list> <topic-name> <out-dir>")
+      sys.error(
+        "Usage: spark-submit --class Main --master <master> streaming.jar <brocker-list> <topic-name> <out-dir>"
+      )
   }
 }
