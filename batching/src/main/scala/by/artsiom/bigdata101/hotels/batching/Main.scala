347ec89cccc90af816cd20c041760e1a6aea61f3
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-26 17:09:26 +0300

    update batching

--- batching/src/main/scala/by/artsiom/bigdata101/hotels/batching/Main.scala
+++ batching/src/main/scala/by/artsiom/bigdata101/hotels/batching/Main.scala
@@ -1,22 +1,17 @@
 package by.artsiom.bigdata101.hotels.batching
-import java.io.ByteArrayInputStream
 
-import by.artsiom.bigdata101.hotels.model.Event
-import com.sksamuel.avro4s.{AvroInputStream, AvroSchema}
-import org.apache.avro.Schema
+import by.artsiom.bigdata101.hotels.HotelImplicits
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.BinaryType
 
-object Main extends App {
+object Main extends App with HotelImplicits {
 
   args match {
     case Array(brockerList, topicName, outputDir) =>
-      val spark = SparkSession.builder
+      implicit val spark = SparkSession.builder
         .master("local[*]")
         .appName("hotels-streaming")
         .getOrCreate()
-
-      val eventCountAcc = spark.sparkContext.longAccumulator("event_count")
 
       val kafkaDF = spark.read
         .format("kafka")
@@ -26,21 +21,11 @@ object Main extends App {
         .load()
 
       import spark.implicits._
-      import Event._
-
-      implicit val eventSchema = AvroSchema[Event]
-
-      val rowEventSchema = eventSchema.toString
 
       kafkaDF
         .coalesce(2)
         .select($"value".cast(BinaryType).as("event"))
-        .map(row => {
-          val bais = new ByteArrayInputStream(row.getAs[Array[Byte]]("event"))
-          val input = AvroInputStream.binary[Event].from(bais).build(new Schema.Parser().parse(rowEventSchema))
-          eventCountAcc.add(1)
-          input.iterator.next()
-        })
+        .fromAvro("event")
         .write
         .format("parquet")
         .option("path", outputDir)
--- interface/src/main/scala/by/artsiom/bigdata101/hotels/model/Event.scala
+++ interface/src/main/scala/by/artsiom/bigdata101/hotels/model/Event.scala
@@ -1,11 +1,6 @@
 package by.artsiom.bigdata101.hotels.model
 
 import java.sql.{Date, Timestamp}
-import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
-
-import com.sksamuel.avro4s.{Decoder, Encoder, SchemaFor}
-import org.apache.avro.Schema
 
 final case class Event(dateTime: Timestamp,
                        siteName: Int,
@@ -31,19 +26,3 @@ final case class Event(dateTime: Timestamp,
                        hotelCountry: Int,
                        hotelMarket: Int,
                        hotelCluster: Int)
-
-object Event {
-  implicit object DateSchemaFor extends SchemaFor[OffsetDateTime] {
-    override val schema: Schema = Schema.create(Schema.Type.STRING)
-  }
-
-  implicit object DateEncoder extends Encoder[OffsetDateTime] {
-    override def encode(t: OffsetDateTime, schema: Schema): AnyRef =
-      t.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-  }
-
-  implicit object DateDecoder extends Decoder[OffsetDateTime] {
-    override def decode(value: Any, schema: Schema): OffsetDateTime =
-      OffsetDateTime.parse(value.toString, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-  }
-}
