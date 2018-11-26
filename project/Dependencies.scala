ec9e8d07fc1f46cc14a80369b3b3a1baffc0394e
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-26 18:37:13 +0300

    refactor dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,18 +7,26 @@ object Dependencies {
     val akkaMonitor         = "0.1.0"
 
     val spark  = "2.4.0"
+    val elasticsearch = "6.5.1"
     val avro4s = "2.0.2"
   }
+  
+  lazy val spark = Seq(
+    "org.apache.spark" %% "spark-core" % versions.spark, // % Provided,
+    "org.apache.spark" %% "spark-sql"  % versions.spark // % Provided
+  )
 
   lazy val common = Seq(
     "com.sksamuel.avro4s" %% "avro4s-kafka" % versions.avro4s,
     "com.sksamuel.avro4s" %% "avro4s-core"  % versions.avro4s
   )
 
-  lazy val sparkCommon = Seq(
-    "org.apache.spark" %% "spark-sql-kafka-0-10" % versions.spark % Provided,
-    "org.apache.spark" %% "spark-core"           % versions.spark % Provided,
-    "org.apache.spark" %% "spark-sql"            % versions.spark % Provided
+  lazy val sparkCommon = spark ++ Seq(
+    "org.apache.spark" %% "spark-sql-kafka-0-10" % versions.spark % Provided
+  )
+  
+  lazy val sparkStreaming = spark ++ Seq(
+    "org.apache.spark" %% "spark-streaming" % versions.spark // % Provided
   )
 
   lazy val generatorModule = Seq(
@@ -27,17 +35,16 @@ object Dependencies {
     "net.ruippeixotog"    %% "akka-stream-mon"       % versions.akkaMonitor
   )
 
-  lazy val streamingModule = Seq(
+  lazy val streamingModule = sparkStreaming ++ Seq(
     "org.apache.spark" %% "spark-streaming-kafka-0-10" % versions.spark,
-    "org.apache.spark" %% "spark-sql-kafka-0-10"       % versions.spark,
-    "org.apache.spark" %% "spark-core"                 % versions.spark, // % Provided,
-    "org.apache.spark" %% "spark-streaming"            % versions.spark, // % Provided,
-    "org.apache.spark" %% "spark-sql"                  % versions.spark // % Provided
+    "org.apache.spark" %% "spark-sql-kafka-0-10"       % versions.spark
   )
 
-  lazy val batchingModule = Seq(
-    "org.apache.spark" %% "spark-sql-kafka-0-10" % versions.spark,
-    "org.apache.spark" %% "spark-core"           % versions.spark, // % Provided,
-    "org.apache.spark" %% "spark-sql"            % versions.spark // % Provided
+  lazy val batchingModule = spark ++ Seq(
+    "org.apache.spark" %% "spark-sql-kafka-0-10" % versions.spark
+  )
+  
+  lazy val elastic = sparkStreaming ++ Seq(
+    "org.elasticsearch" %% "elasticsearch-spark-20" % versions.elasticsearch
   )
 }
