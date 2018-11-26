2fcb25a657137ef6f16f5d85b5651ce9fb61645e
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-26 16:14:33 +0300

    update streaming app

--- streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
+++ streaming/src/main/scala/by/artsiom/bigdata101/hotels/streaming/Main.scala
@@ -1,37 +1,57 @@
 package by.artsiom.bigdata101.hotels.streaming
 
-import org.apache.kafka.common.serialization.StringDeserializer
+import java.io.ByteArrayInputStream
+
+import by.artsiom.bigdata101.hotels.model.Event
+import com.sksamuel.avro4s.{AvroInputStream, AvroSchema}
+import org.apache.avro.Schema
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.BinaryType
 
 object Main extends App {
 
-  val spark = SparkSession.builder
-    .master("local[*]")
-    .appName("hotels-test")
-    .getOrCreate()
+  args match {
+    case Array(brockerList, topicName, outputDir) =>
+      val spark = SparkSession.builder
+        .master("local[*]")
+        .appName("hotels-streaming")
+        .getOrCreate()
 
-  import spark.implicits._
+      val eventCountAcc = spark.sparkContext.longAccumulator("event_count")
 
-  val records = spark.readStream
-    .format("kafka")
-    .option("subscribe", "TestTopic")
-    .option("kafka.bootstrap.servers", "192.168.99.100:9092")
-    .load()
-    .selectExpr("CAST(value AS STRING)")
-    .as[String]
+      val kafkaStreamDF = spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", brockerList)
+        .option("subscribe", topicName)
+        .option("startingOffsets", "latest")
+        .load()
 
-  val sq = records
-    .flatMap(_.split(" "))
-    .groupBy("value")
-    .count()
-    .writeStream
-    .outputMode("complete")
-    .format("console")
-    .start()
+      import spark.implicits._
+      import Event._
 
-  sq.awaitTermination()
-  sq.stop()
+      implicit val eventSchema = AvroSchema[Event]
 
-  spark.stop()
+      val rowEventSchema = eventSchema.toString
+
+      kafkaStreamDF
+        .coalesce(2)
+        .select($"value".cast(BinaryType).as("event"))
+        .map(row => {
+          val bais = new ByteArrayInputStream(row.getAs[Array[Byte]]("event"))
+          val input = AvroInputStream.binary[Event].from(bais).build(new Schema.Parser().parse(rowEventSchema))
+          eventCountAcc.add(1)
+          input.iterator.next()
+        })
+        .writeStream
+        .format("parquet")
+        .queryName("parquet_files_to_hdfs")
+        .outputMode(OutputMode.Append())
+        .option("path", outputDir)
+        .option("checkpointLocation", s"$outputDir/checkpoint")
+        .start()
+        .awaitTermination()
+    case _ =>
+      sys.error("Usage: spark-submit --class Main --master <master> streaming.jar <brocker-list> <topic-name> <out-dir>")
+  }
 }
