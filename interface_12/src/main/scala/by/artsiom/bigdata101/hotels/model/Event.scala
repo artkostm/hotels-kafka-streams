3ed9f622c261202a389e816311d2c18c1aad9a2b
Artsiom <artkostm@gmail.com>
2018-11-27 13:03:56 +0300

    remove project matrix

---
+++ interface_12/src/main/scala/by/artsiom/bigdata101/hotels/model/Event.scala
@@ -0,0 +1,28 @@
+package by.artsiom.bigdata101.hotels.model
+
+import java.sql.{Date, Timestamp}
+
+final case class Event(dateTime: Timestamp,
+                       siteName: Int,
+                       posaContinent: Int,
+                       userLocationCountry: Int,
+                       userLocationRegion: Int,
+                       userLocationCity: Int,
+                       origDestinationDistance: Double,
+                       userId: Int,
+                       isMobile: Boolean,
+                       isPackage: Boolean,
+                       channel: Int,
+                       srchCi: Date,
+                       srchCo: Date,
+                       srchAdultsCnt: Int,
+                       srchChildrenCnt: Int,
+                       srchRmCnt: Int,
+                       srchDestinationId: Int,
+                       srchDestinationTypeId: Int,
+                       isBooking: Boolean,
+                       cnt: Int,
+                       hotelContinent: Int,
+                       hotelCountry: Int,
+                       hotelMarket: Int,
+                       hotelCluster: Int)
