3f7af0a5cec5a7fab601f6cd339465e3a9efe532
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-27 17:08:10 +0300

    update elastic and batch jobs

--- batching/src/main/scala/by/artsiom/bigdata101/hotels/batching/Main.scala
+++ batching/src/main/scala/by/artsiom/bigdata101/hotels/batching/Main.scala
@@ -4,34 +4,35 @@ import by.artsiom.bigdata101.hotels.HotelImplicits
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.BinaryType
 
-object Main extends App with HotelImplicits {
+object Main extends HotelImplicits {
 
-  args match {
-    case Array(brokerList, topicName, outputDir) =>
-      implicit val spark = SparkSession.builder
-        .master("local[*]")
-        .appName("hotels-streaming")
-        .getOrCreate()
+  def main(args: Array[String]): Unit =
+    args match {
+      case Array(brokerList, topicName, outputDir) =>
+        implicit val spark = SparkSession.builder
+          .master("local[*]")
+          .appName("hotels-streaming")
+          .getOrCreate()
 
-      val kafkaDF = spark.read
-        .format("kafka")
-        .option("kafka.bootstrap.servers", brokerList)
-        .option("subscribe", topicName)
-        .option("startingOffsets", "earliest")
-        .load()
+        val kafkaDF = spark.read
+          .format("kafka")
+          .option("kafka.bootstrap.servers", brokerList)
+          .option("subscribe", topicName)
+          .option("startingOffsets", "earliest")
+          .load()
 
-      import spark.implicits._
+        import spark.implicits._
 
-      kafkaDF
-        .coalesce(2)
-        .select($"value".cast(BinaryType).as("event"))
-        .fromAvro("event")
-        .write
-        .format("parquet")
-        .option("path", outputDir)
-        .option("checkpointLocation", s"$outputDir/checkpoint")
-        .save()
-    case _ =>
-      sys.error("Usage: spark-submit --class Main --master <master> batching.jar <brocker-list> <topic-name> <out-dir>")
-  }
+        kafkaDF
+          .coalesce(2)
+          .select($"value".cast(BinaryType).as("event"))
+          .fromAvro("event")
+          .write
+          .format("parquet")
+          .option("path", outputDir)
+          .option("checkpointLocation", s"$outputDir/checkpoint")
+          .save()
+      case _ =>
+        sys.error("Usage: spark-submit --class Main --master <master> batching.jar <brocker-list> <topic-name> <out-dir>")
+    }
 }
--- elastic/src/main/scala/by/artsiom/bigdata101/hotels/elastic/Main.scala
+++ elastic/src/main/scala/by/artsiom/bigdata101/hotels/elastic/Main.scala
@@ -5,41 +5,43 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.types.BinaryType
 
-object Main extends App with HotelImplicits {
+object Main extends HotelImplicits {
 
-  args match {
-    case Array(brokerList, topicName, outputDir) =>
-      implicit val spark = SparkSession.builder
-        .master("local[*]")
-        .appName("hotels-streaming")
-        .getOrCreate()
+  def main(args: Array[String]): Unit =
+    args match {
+      case Array(brokerList, topicName, indexAndType, elasticNodes) =>
+        implicit val spark = SparkSession.builder
+          .master("local[*]")
+          .appName("hotels-streaming")
+          .getOrCreate()
 
-      val kafkaStreamDF = spark.readStream
-        .format("kafka")
-        .option("kafka.bootstrap.servers", brokerList)
-        .option("subscribe", topicName)
-        .option("startingOffsets", "latest")
-        .load()
+        val kafkaStreamDF = spark.readStream
+          .format("kafka")
+          .option("kafka.bootstrap.servers", brokerList)
+          .option("subscribe", topicName)
+          .option("startingOffsets", "latest")
+          .load()
 
-      import spark.implicits._
+        import spark.implicits._
 
-      kafkaStreamDF
-        .coalesce(2)
-        .select($"value".cast(BinaryType).as("event"))
-        .fromAvro("event")
-        .writeStream
-        .format("es")
-        .queryName("stream_to_elastic")
-        .outputMode(OutputMode.Append)
-        .option("es.nodes", "localhost")
-        .option("es.nodes.wan.only", "true")
-        .option("es.index.auto.create", "true")
-        .option("checkpointLocation", s"$outputDir/checkpoint")
-        .start(outputDir)
-        .awaitTermination()
-    case _ =>
-      sys.error(
-        "Usage: spark-submit --class Main --master <master> elastic.jar <brocker-list> <topic-name> <out-dir>"
-      )
-  }
+        kafkaStreamDF
+          .coalesce(2)
+          .select($"value".cast(BinaryType).as("event"))
+          .fromAvro("event")
+          .writeStream
+          .format("es")
+          .queryName("stream_to_elastic")
+          .outputMode(OutputMode.Append)
+          .option("es.nodes", elasticNodes)
+          .option("es.nodes.wan.only", "true")
+          .option("es.mapping.date.rich", "false")
+          .option("es.index.auto.create", "true")
+          .option("checkpointLocation", "checkpoint")
+          .start(indexAndType)
+          .awaitTermination()
+      case _ =>
+        sys.error(
+          "Usage: spark-submit --class Main --master <master> elastic.jar <brocker-list> <topic-name> <[index]/[type]> <elastic-nodes>"
+        )
+    }
 }
