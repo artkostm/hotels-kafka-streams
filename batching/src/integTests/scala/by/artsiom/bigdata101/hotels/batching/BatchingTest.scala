e0680d40d8c724a3ba953b10c794ef32e54b9681
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-28 23:06:16 +0300

    working test

--- batching/src/integTests/scala/by/artsiom/bigdata101/hotels/batching/BatchingTest.scala
+++ batching/src/integTests/scala/by/artsiom/bigdata101/hotels/batching/BatchingTest.scala
@@ -1,5 +1,9 @@
 package by.artsiom.bigdata101.hotels.batching
 
+import java.io.File
+import java.nio.file.{Files, Path}
+import java.util.UUID
+
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
@@ -10,10 +14,12 @@ import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.spark.sql.SparkSession
 import org.scalatest.FlatSpecLike
 import by.artsiom.bigdata101.hotels.generator.publisher.RandomEventsPublisher
+import org.apache.commons.io.FileUtils
 import org.apache.kafka.common.serialization.ByteArraySerializer
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.collection.JavaConverters._
 
 class BatchingTest extends TestKit(ActorSystem("batching_test")) with FlatSpecLike with EmbeddedKafka {
   import BatchingTest._
@@ -23,8 +29,13 @@ class BatchingTest extends TestKit(ActorSystem("batching_test")) with FlatSpecLi
   implicit val kafkaConfig = EmbeddedKafkaConfig(kafkaPort = 12345, zooKeeperPort = 6543)
   implicit val kafkaSerializer = new ByteArraySerializer()
 
-  def withConfig(kafkaConf: EmbeddedKafkaConfig)(test: Config => Unit): Unit =
-    test(Config(s"localhost:${kafkaConf.kafkaPort}", Topic, TempDirectory))
+  def withConfig(kafkaConf: EmbeddedKafkaConfig)(test: Config => Unit): Unit = {
+    val tmpDir = FileUtils.getTempDirectoryPath + File.separator + BatchingTest.TempDirectory + UUID.randomUUID().toString
+    println("Temp file path: " + tmpDir)
+    try test(Config(s"localhost:${kafkaConf.kafkaPort}", Topic, tmpDir))
+    finally FileUtils.deleteQuietly(new File(tmpDir))
+  }
+
 
   it should "be good" in withConfig(kafkaConfig) { config =>
     withRunningKafka {
@@ -32,18 +43,19 @@ class BatchingTest extends TestKit(ActorSystem("batching_test")) with FlatSpecLi
         .map(EventConverter(Topic))
         .runWith(Sink.foreach(msg => publishToKafka(msg)))
 
-      assert(Await.result(messagesPublished, 5 seconds) == Done)
+      assert(Await.result(messagesPublished, 10 seconds) == Done)
 
       Main.run(config)(SparkSession.builder.appName("batching-integ-test").master("local").getOrCreate())
 
-      Thread.sleep(100000)
+      val files = Files.list(new File(config.outputDir).toPath).iterator().asScala
+      assert(files.exists(_.getFileName.toString == "_SUCCESS"))
     }
   }
 }
 
 object BatchingTest {
   val Topic = "TopicTest"
-  val TempDirectory = "tmp"
+  val TempDirectory = "tmp-batching"
   val Parallelism = 10
   val NumberOfEvents = 10
 }
