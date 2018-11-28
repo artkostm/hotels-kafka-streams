eae91f9f9ccc5038572d11db10e435af3f277b2e
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-28 13:12:28 +0300

    add test

--- elastic/src/integTests/scala/by/artsiom/bugdata101/hotels/elastic/TestSpec.scala
+++ elastic/src/integTests/scala/by/artsiom/bugdata101/hotels/elastic/TestSpec.scala
@@ -1,3 +1,8 @@
-package by.artsiom.bugdata101.hotels.elastic class TestSpec {
+package by.artsiom.bugdata101.hotels.elastic
+import org.scalatest.FlatSpec
 
+class TestSpec extends FlatSpec {
+  it should "be good" in {
+    assert(1 == 1)
+  }
 }
--- elastic/src/test/scala/by/artsiom/bigdata101/hotels/elastic/UnitSpec.scala
+++ elastic/src/test/scala/by/artsiom/bigdata101/hotels/elastic/UnitSpec.scala
@@ -1,3 +1,8 @@
-package by.artsiom.bigdata101.hotels.elastic class UnitSpec {
+package by.artsiom.bigdata101.hotels.elastic
+import org.scalatest.FlatSpec
 
+class UnitSpec extends FlatSpec {
+  it should "be great" in {
+    assert(2 == 2)
+  }
 }
