e52b84eccbb1d149b67aecc0ab41e606368a812f
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-28 14:04:36 +0300

    add hotels app

---
+++ spark-common/src/main/scala/by/artsiom/bigdata101/hotels/HotelsApp.scala
@@ -0,0 +1,15 @@
+package by.artsiom.bigdata101.hotels
+import org.apache.spark.sql.SparkSession
+
+trait HotelsApp[T] extends Serializable {
+
+  def run(spark: SparkSession, config: T): Unit
+  def setup(args: Array[String]): Either[InitError, (SparkSession, T)]
+
+  def main(args: Array[String]): Unit =
+    setup(args).fold(error => sys.error(error.usage), {
+      case (spark, config) => run(spark, config)
+    })
+}
+
+final case class InitError(usage: String)
