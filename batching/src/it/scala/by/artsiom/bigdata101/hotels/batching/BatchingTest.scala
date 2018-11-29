0ad023904b092dcbb144a19028eea4ae2291f388
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-29 17:23:25 +0300

    formatting

--- batching/src/it/scala/by/artsiom/bigdata101/hotels/batching/BatchingTest.scala
+++ batching/src/it/scala/by/artsiom/bigdata101/hotels/batching/BatchingTest.scala
@@ -21,41 +21,49 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.collection.JavaConverters._
 
-class BatchingTest extends TestKit(ActorSystem("batching_test")) with FlatSpecLike with EmbeddedKafka {
+class BatchingTest
+    extends TestKit(ActorSystem("batching_test"))
+    with FlatSpecLike
+    with EmbeddedKafka {
   import BatchingTest._
 
   implicit val mat = ActorMaterializer()
 
-  implicit val kafkaConfig = EmbeddedKafkaConfig(kafkaPort = 12345, zooKeeperPort = 6543)
+  implicit val kafkaConfig     = EmbeddedKafkaConfig(kafkaPort = 12345, zooKeeperPort = 6543)
   implicit val kafkaSerializer = new ByteArraySerializer()
 
   def withConfig(kafkaConf: EmbeddedKafkaConfig)(test: Config => Unit): Unit = {
-    val tmpDir = FileUtils.getTempDirectoryPath + File.separator + BatchingTest.TempDirectoryPrefix + UUID.randomUUID().toString
+    val tmpDir = FileUtils.getTempDirectoryPath + File.separator + BatchingTest.TempDirectoryPrefix + UUID
+      .randomUUID()
+      .toString
     println("Temp file path: " + tmpDir)
     try test(Config(s"localhost:${kafkaConf.kafkaPort}", Topic, tmpDir))
     finally FileUtils.deleteQuietly(new File(tmpDir))
   }
 
+  it should "successfully create parquet files from kafka messages" in withConfig(kafkaConfig) {
+    config =>
+      withRunningKafka {
+        val messagesPublished = Source
+          .fromPublisher(RandomEventsPublisher(10))
+          .map(EventConverter(Topic))
+          .runWith(Sink.foreach(msg => publishToKafka(msg)))
 
-  it should "successfully create parquet files from kafka messages" in withConfig(kafkaConfig) { config =>
-    withRunningKafka {
-      val messagesPublished = Source.fromPublisher(RandomEventsPublisher(10))
-        .map(EventConverter(Topic))
-        .runWith(Sink.foreach(msg => publishToKafka(msg)))
+        assert(Await.result(messagesPublished, 10 seconds) == Done)
 
-      assert(Await.result(messagesPublished, 10 seconds) == Done)
+        Main.run(config)(
+          SparkSession.builder.appName("batching-integ-test").master("local").getOrCreate()
+        )
 
-      Main.run(config)(SparkSession.builder.appName("batching-integ-test").master("local").getOrCreate())
-
-      val files = Files.list(new File(config.outputDir).toPath).iterator().asScala
-      assert(files.exists(_.getFileName.toString == "_SUCCESS"))
-    }
+        val files = Files.list(new File(config.outputDir).toPath).iterator().asScala
+        assert(files.exists(_.getFileName.toString == "_SUCCESS"))
+      }
   }
 }
 
 object BatchingTest {
-  val Topic = "TopicTest"
+  val Topic               = "TopicTest"
   val TempDirectoryPrefix = "tmp-batching-"
-  val Parallelism = 10
-  val NumberOfEvents = 10
+  val Parallelism         = 10
+  val NumberOfEvents      = 10
 }
